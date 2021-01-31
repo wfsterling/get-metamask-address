50a42efd1ad86c6517c7b6f233699697c9e47ea5
William Sterling <sterlinw@b19945.local>
2021-01-30 21:46:06 -0700

    connecting to Metamask

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Web3 from 'web3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const network = await web3.eth.net.getNetworkType()
+    // Fetch Account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({account: accounts[0] })
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { account: '' };
+
+  }
+
+  render() {
+    return (
+      <Container>
+        <Typography variant="h1">
+          Hello World
+        </Typography>
+        <Typography variant="h5">
+          Your account: {this.state.account}
+        </Typography>
+      </Container>
+    )
+  }
 }
+
+
 
 export default App;
